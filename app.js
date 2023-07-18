b71f7a78ad7f957c80c11f0dc4183ddbedb129b5
Konstantin Bobkov <mrglorf@gmail.com>
2023-07-18 07:45:41 +0400

    add signin and signup, protect routes

--- app.js
+++ app.js
@@ -4,19 +4,28 @@ const mongoose = require('mongoose');
 
 const app = express();
 const bodyParser = require('body-parser');
+const { login, createUser } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  autoIndex: true, //make this also true
+});
 
+app.post('/signin', login);
+app.post('/signup', createUser);
 app.use((req, res, next) => {
   req.user = {
-    _id: '64a8cd357a5cd296d5bb4e8f',
+    _id: '64b5f08b31970ff788b38362',
   };
 
   next();
 });
+app.use(auth);
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,6 @@
+const bcrypt = require('bcrypt');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 module.exports.getUsers = (req, res) => {
   User.find({}).then((users) => {
@@ -15,9 +17,12 @@ module.exports.getUser = (req, res) => {
       } else {
         res.send(user);
       }
-    }).catch((err) => {
+    })
+    .catch((err) => {
       if (err.name === 'CastError') {
-        res.status(400).send({ message: 'Неверный формат идентификатора пользователя' });
+        res
+          .status(400)
+          .send({ message: 'Неверный формат идентификатора пользователя' });
       } else {
         res.status(500).send({ message: 'На сервере произошла ошибка' });
       }
@@ -25,8 +30,12 @@ module.exports.getUser = (req, res) => {
 };
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
+  const { name, about, avatar, email, password } = req.body;
+  bcrypt
+    .hash(password, 10)
+    .then((hash) => {
+      return User.create({ name, about, avatar, email, password: hash });
+    })
     .then((user) => {
       res.send({ user });
     })
@@ -39,7 +48,7 @@ module.exports.createUser = (req, res) => {
         res.status(500).send({ message: 'На сервере произошла ошибка' });
       }
     });
-};
+}; // проверить чтобы сервер сообщал что пользователь с таким имейлом уже существует
 
 module.exports.updateUserInfo = (req, res) => {
   const { name, about } = req.body;
@@ -50,7 +59,7 @@ module.exports.updateUserInfo = (req, res) => {
     {
       new: true,
       runValidators: true,
-    },
+    }
   )
     .then((user) => {
       res.send({ user });
@@ -75,7 +84,7 @@ module.exports.updateUserAvatar = (req, res) => {
     {
       new: true,
       runValidators: true,
-    },
+    }
   )
     .then((user) => {
       res.send(user);
@@ -88,5 +97,32 @@ module.exports.updateUserAvatar = (req, res) => {
       } else {
         res.status(500).send({ message: 'На сервере произошла ошибка' });
       }
+    });
+};
+
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      } else {
+        return bcrypt.compare(password, user.password).then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          } else {
+            return user;
+          }
+        });
+      }
+    })
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, 'super-strong-secret', {
+        expiresIn: '7d',
+      });
+      res.send({ token: token });
+    })
+    .catch((err) => {
+      res.status(401).send({ message: err.message });
     });
 };
