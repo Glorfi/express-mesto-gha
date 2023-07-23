a09f61eaa5a11676a48eb868137a6b6ded0d0ed9
Konstantin Bobkov <mrglorf@gmail.com>
2023-07-23 06:45:11 +0400

    fix not found issue

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const app = express();
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const handleErrors = require('./middlewares/handleErrors');
-const handleErrors = require('./middlewares/handleErrors');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
