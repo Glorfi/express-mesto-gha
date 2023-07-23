57a8f84e19b41a1f67fa7a79b99ae25edd506c71
Konstantin Bobkov <mrglorf@gmail.com>
2023-07-23 06:07:52 +0400

    quick fix for error server responce

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const {
   DuplicatedEmail,
   NotFound,
   WrongFormat,
+  NotAuthorized,
 } = require('./utils/errorsConfig');
 
 app.use(bodyParser.json());
@@ -48,6 +49,9 @@ app.use((err, req, res, next) => {
   if (err instanceof NotFound) {
     res.status(err.statusCode).json({ message: err.message });
   }
+  if (err instanceof NotAuthorized) {
+    res.status(err.statusCode).json({ message: err.message });
+  }
   if (err.name === 'CastError') {
     const e = new WrongFormat('Неверный формат идентификатора');
     res.status(e.statusCode).json({ message: e.message });
@@ -55,6 +59,9 @@ app.use((err, req, res, next) => {
   if (err.name === 'ValidationError') {
     const e = new WrongFormat('Неверный формат передаваемых данных');
     res.status(e.statusCode).json({ message: e.message });
+  }
+  if (err.status === 500) {
+    res.status(404).json({ message: 'Внутреняя ошибка сервера' });
   }
 });
 
