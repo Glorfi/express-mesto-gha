f4210ce148ceabe714dce3449fefca3882ff20aa
Konstantin Bobkov <mrglorf@gmail.com>
2023-07-24 16:25:55 +0400

    fix after review 2

--- middlewares/handleErrors.js
+++ middlewares/handleErrors.js
@@ -1,17 +1,5 @@
-const AuthorizationRequired = require('../utils/AuthorizationRequired');
-const DuplicatedEmail = require('../utils/DuplicatedEmail');
-const NotAuthorized = require('../utils/NotAuthorized');
-const NotFound = require('../utils/NotFound');
-const WrongFormat = require('../utils/WrongFormat');
-
 module.exports = (err, req, res, next) => {
-  if (
-    err instanceof DuplicatedEmail
-    || err instanceof NotFound
-    || err instanceof NotAuthorized
-    || err instanceof AuthorizationRequired
-    || err instanceof WrongFormat
-  ) {
+  if (err.statusCode) {
     res.status(err.statusCode).json({ message: err.message });
   } else {
     res.status(500).json({ message: 'Внутреняя ошибка сервера' });
--- utils/WrongFormat.js
+++ utils/WrongFormat.js
@@ -1,7 +1,7 @@
 class WrongFormat extends Error {
   constructor(message) {
     super(message);
-    this.statusCode = 456;
+    this.statusCode = 400;
   }
 }
 
